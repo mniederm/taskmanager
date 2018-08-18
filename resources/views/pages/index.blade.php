4a87234630dccdcf97ea25fcb93b4fe5ff836222
Michael Niedermoser <michaelniedermoser@Michaels-MacBook-Pro-2.local>
2018-08-18 13:07:14 +0200

    Navbar, Pages basic setup, css and js changes

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/app')
+
+@section('content')
+    <h1>Welcome to {{config('app.name', 'Taskmanager')}}</h1> 
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam libero cum odio dignissimos reprehenderit rem magni animi hic tempora perspiciatis, voluptates cupiditate ex placeat molestiae. Beatae provident omnis temporibus inventore!</p>  
+@endsection
+
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/app')
+
+@section('content')
+    <h1>Services of {{config('app.name', 'Taskmanager')}}</h1> 
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam libero cum odio dignissimos reprehenderit rem magni animi hic tempora perspiciatis, voluptates cupiditate ex placeat molestiae. Beatae provident omnis temporibus inventore!</p>  
+@endsection
